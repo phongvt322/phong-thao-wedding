9ee553570cb790dbf5a928f4908bb52a2a6cbdc3
phong.vt <phong.vt@ssv.com.vn>
2024-11-20 17:27:11 +0700

    fix duplicate

--- js/scripts.js
+++ js/scripts.js
@@ -258,7 +258,7 @@ $(document).ready(function () {
       $("#alert-wrapper").html(alert_markup("danger", "<strong>123</strong>"));
     } else {
       $.ajax({
-        url: "https://script.google.com/macros/s/AKfycbz6kPHvztMeWgKbjvN1xKHfEXUp-tBsyMXisUn_S5_8viyZKIsRVEOukK-RWXwux3cRvA/exec",
+        url: "https://script.google.com/macros/s/AKfycbxAsIDBxSLVj-s-b8HyFpxD-1lKTkflSaghRIzJtyyiCyIdLa-9_RXlY_FFiERDoMXiVQ/exec",
         type: "POST",
         data: data,
         contentType: "application/x-www-form-urlencoded",
